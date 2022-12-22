9fad2a47360442370c66ae0fddec633e0f089dbd
MosehMaguah <Mosehmaguah@gmail.com>
2022-12-22 15:07:50 +0300

    print numbers

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ * @n: the integer to be printed
+ * Return: 0
+ */
+
+void print_number(int n)
+{
+	unsigned int num = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -n;
+	}
+
+	if ((num / 10) > 0)
+		print_num(num / 10);
+	_putchar((num % 10) + '0');
+}

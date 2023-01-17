85a0c5ff4d60656a13bc76a0b6aa9f46caaa8146
MosehMaguah <Mosehmaguah@gmail.com>
2023-01-17 19:22:17 +0300

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+#include "main.h"
+
+/**
+  * struct dog - struct dog
+  * @name: name
+  * @age: age
+  * @owner: owner
+  * Description: this struct is for dog
+  */
+
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+}
